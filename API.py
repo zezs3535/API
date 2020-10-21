4eba40d4faf9a823a1069da57032f9693450b57e
zezs3535 <zezs3535@naver.com>
2020-10-21 14:57:27 +0900

    Django

--- API.py
+++ API.py
@@ -10,7 +10,7 @@
 
 # for i in soup.find_all('price'):
 #     print(i)
-    
+
 # print(res.status_code)
 # if res.status_code==200:
 #     print("요청 성공")
@@ -23,15 +23,16 @@ from urllib.request import urlopen
 from urllib.parse import quote_plus
 import time
 
-if __name__=="__main__":
-    keyword=input("검색할 키워드를 입력하세요 : ")
-    
-    url=requests.get("https://search.naver.com/search.naver?where=news&sm=tab_jum&query=%s"%keyword)
+if __name__ == "__main__":
+    keyword = input("검색할 키워드를 입력하세요 : ")
 
-    soup=BeautifulSoup(url.text,'html.parser')
-    title=soup.select('.news .type01 li dt a')
-    uploadTime=soup.select('.news .type01 li dd span')
-    f=open("News.txt","w")
+    url = requests.get(
+        "https://search.naver.com/search.naver?where=news&sm=tab_jum&query=%s" % keyword)
+
+    soup = BeautifulSoup(url.text, 'html.parser')
+    title = soup.select('.news .type01 li dt a')
+    uploadTime = soup.select('.news .type01 li dd span')
+    f = open("News.txt", "w")
     for i in title:
         f.write(i.attrs['title'])
         f.write("\nURL : ")
